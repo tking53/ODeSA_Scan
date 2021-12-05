981c783bece78377aac06698386a41f83378cfb0
T. T. King <tking.91@gmail.com>
2021-12-04 20:53:40 -0500

    LARGE updates to actual scanning source code
    Code is complete. it seems to work, what i get is reasonable, however it needs to be validated

--- ODeSA_Scan.cpp
+++ ODeSA_Scan.cpp
@@ -25,27 +25,106 @@
 
 #include "ODeSA_Scan.hpp"
 #include <string.h>
+#include <iterator>
+
+#include "ODeSA_Scan.hpp"
 
 using namespace std;
-#include "ODeSA_Scan.hpp"
 
 int main(int argc, char *argv[]) {
 
     ODeSA_Scan scan; 
+    PulseAnalysis *Analysis = new PulseAnalysis();
 
-   scan.ParseCmdFlags(argc, argv);
+    int FlagReturn = scan.ParseCmdFlags(argc, argv);
+    if (FlagReturn != 0) {
+        return FlagReturn;
 
-    // cout<< " checking cmd flags" << endl;
-    // cout<< " Number of Files= "<< scan.GetNumberOfFiles() <<endl;
-    // cout<< " BatchMode= " << scan.GetBatchMode() <<endl;
-    // cout<< " DebugMode= " << scan.GetDebugMode() <<endl;
-    // cout<< " InputFile= " << scan.GetInputFileName() << endl;
-    // cout<< " OutputFile= "<< scan.GetOutputFileName() <<endl;
-    // cout<< " Header= " << scan.GetOutputFileHeader() << endl;
+    } else {
 
-    //return Scan(numberOfFiles_, batchMode_, debugMode_, outName_, inName_, header_ );
+        cout << "\n checking cmd flags" << endl;
+        cout << " Number of Files= " << scan.GetNumberOfFiles() << endl;
+        cout << " BatchMode= " << scan.GetBatchMode() << endl;
+        cout << " DebugMode= " << scan.GetDebugMode() << endl;
+        cout << " VerboseMode= " << scan.GetVerboseTerminalErrors() << endl;
+        cout << " InputFile= " << scan.GetInputFileName() << endl;
+        cout << " OutputFile= " << scan.GetOutputFileName() << endl;
+        cout << " Header= " << scan.GetOutputFileHeader() << endl
+             << endl
+             << endl;
+
+        scan.PrintStartMessage();
+
+        vector<int> listOfFiles;
+        map<int, ifstream *> mapToFilePointers = scan.OpenInputFiles(scan.GetInputFileName(), scan.GetNumberOfFiles(),listOfFiles);
+
+        TTree *RootTree = scan.InitOutput(scan.GetOutputFileName(), scan.GetOutputFileHeader(), scan.GetNumberOfFiles(),listOfFiles);
+
+        bool dataLeftToProcess = true;
+        long eventNumber = 0;
+
+        while (dataLeftToProcess) {
+            double curTime;
+            // cout<<"While loop"<<endl;
+            // (scan.GetDetectorMap()).erase(scan.GetDetectorMap().begin(),scan.GetDetectorMap().end());
+            // scan.GetDetectorMap().clear();
+            // cout<<"DetMap.size()= "<<scan.GetDetectorMap()->size()<<endl;
+           
+            for (auto itfile = mapToFilePointers.begin(); itfile != mapToFilePointers.end(); ++itfile) {
+                ODeSA_Scan::LiquidCan temporaryDetector;
+                if (itfile->second == NULL) {
+                    // detVec.at(itfile->first) =
+                    continue;
+                } else {
+                    int processingRetVal = scan.ProcessSingleEvent(itfile->second, Analysis, temporaryDetector, itfile->first, curTime, eventNumber, dataLeftToProcess);
+
+                    if (processingRetVal == 0) {
+                        if (scan.GetDetectorMap()->find(itfile->first) == scan.GetDetectorMap()->end()) {
+                            cout << "ERROR:: Failed to find detecotor map key for updating. This is after the file processed" << endl;
+                            break;
+                        } else {
+                            scan.GetDetectorMap()->find(itfile->first)->second = temporaryDetector;
+                        }
+                        // x.second = temporaryDetector;
+                        // cout << "tempDetectorSruct.l= " << temporaryDetector.l << endl;
+                        // ;
+                        // cout << "detmap.find("<<itfile->first<<").l= " << scan.GetDetectorMap()->find(itfile->first)->second.l << endl;
+                        
+                        
+                        // cout << "second detmap.find(" << itfile->first << ").l= " << scan.GetDetectorMap()->find(itfile->first)->second.l << endl;
+
+                        // // cout<< endl<<" after set"<<endl;
+                        // // cout<<"tempDetector.l= " << temporaryDetector.l <<endl;
+                        // // cout<<"detmap.l= " << (scan.GetDetectorMap().at(itfile->first)).l << endl << endl;
+                    }
+                }
+                
+                if (itfile->first == 0) {
+                    // Runtime clock
+                    double difftime = curTime - scan.GetPreviousTime();
+                    if (difftime < 0) {
+                        difftime = difftime + 2147483647;
+                        scan.SetRunTime(scan.GetRunTime() + ((8 * difftime) / 1.0E6));
+                        scan.SetPreviousTime(curTime);
+                    } else {
+                        scan.SetRunTime(scan.GetRunTime() + ((8 * difftime) / 1.0E6));
+                        scan.SetPreviousTime(curTime);
+                    }
+                }
+            }
+            // dataLeftToProcess=false;
+            // cout<<"Prefill"<<endl;
+            RootTree->Fill();
+            ++eventNumber;
+            // cout<<"PostFile"<<endl;
+            if (eventNumber % 1000 == 0) {
+                cout << "\rEvent counter: " << eventNumber << flush;
+            }
+        }
+        cout<<endl<<"Scanning is finished. Check root for cfd= \"-99990\" || \"-99991\" which means the CFD failed for those events."<<endl;
+        return 0;
+    }
 }
-
 int ODeSA_Scan::ParseCmdFlags(int argc, char *argv[]) {
     int idx = 0;
     int retval = 0;
@@ -58,11 +137,12 @@ int ODeSA_Scan::ParseCmdFlags(int argc, char *argv[]) {
         {"header", required_argument, NULL, 'h'},
         {"input", required_argument, NULL, 'o'},
         {"debug", no_argument, NULL, 'd'},
+        {"verbose", optional_argument, NULL, 'v'},
         {"help", no_argument, NULL, '?'},
         {NULL, no_argument, NULL, 0}};
 
     if (argc != 0) {
-        while ((retval = getopt_long(argc, argv, "n:bh:i:o:d", longOpts, &idx)) != -1) {
+        while ((retval = getopt_long(argc, argv, "n:bh:i:o:dv", longOpts, &idx)) != -1) {
             switch (retval) {
                 case 'n':
                     if (optarg != 0) {
@@ -79,9 +159,12 @@ int ODeSA_Scan::ParseCmdFlags(int argc, char *argv[]) {
                 case 'd':
                     SetDebugMode(true);
                     break;
+                case 'v':
+                    SetVerboseTerminalErrors(true);
+                    break;
                 case 'o':
                     if (optarg != 0) {
-                        SetOutputFileName(optarg);
+                        SetOutputFileName(optarg, ".root");
                     } else {
                         cout << "ERROR:: Missing Output root file name." << endl;
                         return 1;
@@ -119,7 +202,7 @@ int ODeSA_Scan::ParseCmdFlags(int argc, char *argv[]) {
     return 0;
 }
 
-void ODeSA_Scan::InitOutput(string outputFileName_, string outputFileHeader_, int numFiles_) {
+TTree* ODeSA_Scan::InitOutput(string outputFileName_, string outputFileHeader_, int numFiles_, vector <int> listOfFiles_) {
     
     // static Can d0,d1,d2,d3,d4,d5,d6,d7
 
@@ -128,18 +211,284 @@ void ODeSA_Scan::InitOutput(string outputFileName_, string outputFileHeader_, in
 
     tt->Branch("runtime", &runtime, "Runtime (ms)");  // Runtime in ms
 
-    for (unsigned i = 0; i < numFiles_; ++i) {
-        Can detector;
-        detVec.emplace_back( detector );
-        string key = "d" + to_string(i);
-        tt->Branch(key.c_str(), &(detVec.at(i)), "l:s:amp:cfd:psd:trig:pp");
-        
+    for (unsigned i = 0; i < listOfFiles_.size(); ++i) {
+        ODeSA_Scan::LiquidCan detector;
+        string key = "d" + to_string(listOfFiles_.at(i));
+        detMap.emplace( listOfFiles_.at(i), detector );
+        tt->Branch(key.c_str(), &(detMap.at(listOfFiles_.at(i))), "l:s:amp:ampRaw:cfd:psd:pp/F:trig/O");
         if (GetDebugMode()){
-            string traceKey_ = "trace" + to_string(i) + "_d";
-            string traceTitle_ = "Debugging Traces for Channel " + to_string(i);
-           // debug_Trace_vector.emplace_back(new TH1F(traceKey_.c_str(), traceTitle_.c_str(), 2000, 0, 1999));
-            //tt->Branch(traceKey_.c_str(), "TH1F", &(debug_Trace_vector.at(i)));
+            string traceKey_ = "trace" + to_string(listOfFiles_.at(i)) + "_d";
+            string traceTitle_ = "Debugging Traces for Channel " + to_string(listOfFiles_.at(i));
+            // TH1* tmpDebuggingTrace = new TH1F(traceKey_.c_str(),traceTitle_.c_mstr(),2000,0,1999);
+            debug_Trace_map.emplace(listOfFiles_.at(i), new TH1F(traceKey_.c_str(),traceTitle_.c_str(),2000,0,1999)); 
+            tt->Branch(traceKey_.c_str(), "TH1F", &debug_Trace_map.at(listOfFiles_.at(i)));
         }
     }
+    return tt;
 }
+
+map<int,ifstream*> ODeSA_Scan::OpenInputFiles(string prefix_, int NumOfFiles_, vector<int> &listOfFiles_){
+    string FullFileName;
+    map<int, ifstream *> fileStreams;
+   
+    for( auto fileCounter = 0; fileCounter < NumOfFiles_; ++fileCounter){
+        FullFileName = prefix_ + "_wave" + to_string(fileCounter) + ".dat";
+        
+        ifstream* FilePointer = new ifstream;
+        FilePointer->open(FullFileName.c_str(), std::ifstream::in | std::ifstream::binary);
+        if (FilePointer->is_open()){
+            cout<< "Opening file: " << FullFileName << " -- Open!"<<endl;
+            fileStreams.emplace(fileCounter,FilePointer);
+            listOfFiles_.emplace_back(fileCounter);
+        } else {
+            cout<< "Opening file: " << FullFileName << " -- Failed!"<<endl;
+        }
+    }
+    return fileStreams;
+}
+
+int ODeSA_Scan::ProcessSingleEvent(ifstream *file_, PulseAnalysis *Analysis_, LiquidCan &detector, int chanNum_, double &curTime, long &eventNum_,  bool &gotMoreData, bool doTraceAnalysis, pair<int, int> cfdRange_ , pair<int, int> psdRange_ ) {
+    Float_t amplitude = 0, rawAmplitude=0;
+    Float_t CFD_Phase = 0;
+    int retVal = 0 ;
+
+    bool trig = false;
+   
+    bool gotTraceData = true;
+
+    int pposition = 0;
+    int Tracelength;
+    int eventlength;
+    pair<double, double> psdValues = {0,0};
+
+    // For SG filtered pulse
+    Float_t trace_min, trace_max;
+    int trace_min_loc, trace_max_loc;
+    bool zero_cross;
+
+    // const int preallocated_pulse_size = 2000;
+    vector<float> pulse;
+    vector<float> rawPulse;
+    vector<float> CMAtrace;
+    vector<float> SG_pulse;
+    vector<float> SGderv2_pulse;
+    vector<float> baseline;
+
+    uint32_t buffer32;
+    uint16_t buffer16;
+
+    if (file_->is_open()) {
+        trace_min = 0;
+        if (!file_->read((char *)&buffer32, 4)) {
+            gotMoreData = false;
+            return 1;
+        }
+        Tracelength = (buffer32 - 16) / 2;
+        file_->read((char *)&buffer32, 4);
+        file_->read((char *)&buffer32, 4);
+        file_->read((char *)&buffer32, 4);
+
+        // Trigger time in 2 ADC clock cycles ( 8 ns )
+        if (chanNum_ == 0) {
+            curTime = buffer32;
+        } else {
+            curTime = -1;
+        };
+        // Get traces
+        if (Tracelength == 0) {
+            // no traces
+            retVal = 2;
+        } else {
+            for (int i = 0; i < Tracelength; i++) {
+                if (!file_->read((char *)&buffer16, 2)) {
+                    gotTraceData = false;
+                    break;
+                }
+                if (chanNum_ < 12) {
+                    pulse.emplace_back( 16383 - (float)buffer16);
+                } else {
+                    pulse.emplace_back((float)buffer16);
+                }
+
+                if (pulse.at(i) > (16383 - threshold.at(chanNum_))) {
+                    trig = true;
+                }
+
+                if (GetDebugMode()) {
+                    debug_Trace_map.at(chanNum_)->Fill(i, pulse.at(i));
+                }
+                CMAtrace.emplace_back(0);
+            }
+
+            if (Tracelength > 1) {
+                if (chanNum_ < 10) {
+                    Analysis_->CMA_Filter(pulse.data(), Tracelength, CMAtrace.data(), 10, pulse.at(0), 3.5);
+                } else {
+                    // CMAtrace.fill(CMAtrace.begin(), CMAtrace.end(), 0)
+                    std::fill(CMAtrace.begin(), CMAtrace.end(), 0);
+                }
+
+                for (int it = 0; it < pulse.size(); ++it) {
+                    rawPulse.emplace_back(pulse.at(it));  // MUST BE DONE BEFORE Trace subtraction
+                    pulse.at(it) -= CMAtrace.at(it);  // subtrace trace baseline from full trace
+                }
+
+                //! Find Peak Position and Amplitude from the subtraced trace.
+                if (chanMap.find(chanNum_)->second == "RF" || chanMap.find(chanNum_)->second == "SCATTERING") {  //RF and Scattering detecotrs for LANL2021
+                    //! for these channels I'm excluding the first and last 200ns ( 100 ticks @ 500MHz)
+                    pair<unsigned int, unsigned int> blocking_range = {100, 100};
+                    // vector<float>::iterator beginTest = pulse.begin();
+                    // std::advance(beginTest, 100);
+                    // vector<float>::iterator endTest = pulse.end();
+                    // std::advance(endTest, -100);
+                    // vector<float>::iterator beginTestRaw = rawPulse.begin();
+                    // std::advance(beginTestRaw, 100);
+                    // vector<float>::iterator endTestRaw = rawPulse.end();
+                    // std::advance(endTestRaw, -100);
+
+                    // pposition = std::max_element(beginTest , endTest) - pulse.begin();
+                    // amplitude = *std::max_element(beginTest,endTest);
+                    // rawAmplitude = *std::max_element(beginTestRaw,endTestRaw);
+
+                    pposition = std::max_element(pulse.begin()+blocking_range.first, pulse.end()- blocking_range.second) - pulse.begin();
+                    amplitude = *std::max_element(pulse.begin()+blocking_range.first, pulse.end()- blocking_range.second);
+                    rawAmplitude = *std::max_element(rawPulse.begin()+blocking_range.first, rawPulse.end()- blocking_range.second);
+
+                } else {
+                    pposition = std::max_element(pulse.begin(), pulse.end()) - pulse.begin();
+                    amplitude = *std::max_element(pulse.begin(), pulse.end());
+                    rawAmplitude = *std::max_element(rawPulse.begin(), rawPulse.end());
+                }
+
+                //! Begin Timing Section
+                float CFD_Phase;
+                if (pposition > 10 && pposition < Tracelength - 10) {
+                    if (chanMap.find(chanNum_)->second == "ODESA") {
+                        CFD_Phase = ODeSA_Cfd(cfdRange_, pulse, pposition, amplitude);
+                    } else {
+                        CFD_Phase = Extras_Cfd(cfdRange_, pulse, pposition, amplitude);
+                    }
+                } else {
+                    CFD_Phase = -99991;
+                }
+                if (GetVerboseTerminalErrors()) {
+                    if (CFD_Phase == -99990) {
+                        cout << endl
+                             << "ERROR:: CFD for channel " << chanNum_ << " in event number " << eventNum_ << " failed due to a divide by zero" << endl;
+                        // && pposition > 10 && chanNum_ != 1
+                    } else if (CFD_Phase == -99991) {
+                        cout << endl
+                             << "ERROR:: CFD for channel " << chanNum_ << " in event number " << eventNum_ << " failed due due to bad peak position. Position / Trace Length = " << pposition << " / " << Tracelength << endl;
+                    }
+                }
+
+                //! PSD Stuff
+                if (pposition - psdRange_.first > 0 && pposition + psdRange_.second < Tracelength) {
+                    if (chanMap.find(chanNum_)->second == "ODeSA") {
+                        psdValues = ODeSA_PSD(psdRange_, pulse, pposition);
+                    } else {
+                        psdValues = Extras_PSD(psdRange_, pulse, pposition);
+                    }
+                }
+            }
+        }
+        if (retVal == 0) {
+            pair<float,float> chanCal = calibrations.find(chanNum_)->second;
+            //fill detstruct
+            detector.s = ( chanCal.second * psdValues.first ) + (chanCal.first);
+            detector.l = (chanCal.second * psdValues.second) + (chanCal.first);
+            detector.psd = psdValues.first / psdValues.second;
+            detector.amp = amplitude;
+            detector.ampRaw = rawAmplitude;
+            detector.cfd = CFD_Phase;
+            detector.pp = pposition;
+            detector.trig = trig;
+        }
+        return retVal;
+    } else {
+        return -1;  //if file not open
+    }
+}
+
+int ODeSA_Scan::ODeSA_Cfd(pair<int, int> cfdRange_, vector<float> trace_, int &peakPosition_, float &amplitude ) {
+    int leadingEdge;
+    float xy_ = 0, x_ = 0, y_ = 0, x_2 = 0, m_ = 0, b_ = 0, counter = 0;
+    for (unsigned it = peakPosition_ - 10; it <= peakPosition_; ++it) {
+        if (trace_.at(it) <= 0.5 * amplitude) {
+            leadingEdge = it;
+        }
+    }
+   
+    for (unsigned it = leadingEdge - cfdRange_.first; it <= leadingEdge + cfdRange_.second; ++it) {
+        ++counter;  // we need to know later how many points we looped over
+        x_ += it;
+        x_2 += it * it;
+        y_ += trace_.at(it);
+        xy_ += trace_.at(it) * it;
+    }
+    m_ = ((counter * xy_) - (x_ * y_)) / ((counter * x_2) - (x_ * x_));
+    b_ = ((y_ * x_2) - (x_ * xy_)) / ((counter * x_2) - (x_ * x_));
+
+    if (m_ != 0 ){
+    return (((0.5 * amplitude) - b_) / m_);
+    } else {
+        return -99990;
+    }
+
+};  // end Func
+
+int ODeSA_Scan::Extras_Cfd(pair<int, int> cfdRange_, vector<float> trace_, int &peakPosition_, float &amplitude ) {
+    int leadingEdge;
+    double xy_ = 0, x_ = 0, y_ = 0, x_2 = 0, m_ = 0, b_ = 0, counter = 0;
+    for (unsigned it = peakPosition_ - 10; it<= peakPosition_; ++it) {
+        if (trace_.at(it) <= 0.5 * amplitude) {
+            leadingEdge = it;
+        }
+    }
+
+    
+    for (unsigned it =  leadingEdge - cfdRange_.first; it <= leadingEdge + cfdRange_.second; ++it) {
+        ++counter; // we need to know later how many points we looped over
+        x_ += it;
+        x_2 += it * it;
+        y_ += trace_.at(it);
+        xy_ += trace_.at(it) * it;
+    }
+    m_ = ((counter * xy_) - (x_ * y_)) / ((counter * x_2) - (x_ * x_));
+    b_ = ((y_ * x_2) - (x_ * xy_)) / ((counter * x_2) - (x_ * x_));
+
+    if (m_ == (double)0.0) {
+        return -99990;
+    } else {
+        return (((0.5 * amplitude) - b_) / m_);
+        
+    }
+}
+
+//! <short, Long>
+pair<double, double> ODeSA_Scan::ODeSA_PSD(pair<int, int> psdRange_, vector<float> trace_, int peakPosition_, int offset) {
+    // PSD integration
+    double paraS = 0, paraL = 0;
+    for (unsigned it = (peakPosition_ - psdRange_.first); it < (peakPosition_ + psdRange_.second); ++it) {
+        paraL += trace_.at(it );
+        if (it > peakPosition_ + offset) {
+            paraS += trace_.at(it);
+        }
+    }
+return make_pair(paraS,paraL);
+};
+
+//! <Short, Long>
+pair<double, double> ODeSA_Scan::Extras_PSD(pair<int, int> psdRange_, vector<float> trace_, int peakPosition_, int offset) {
+    // PSD integration
+    double paraS = 0, paraL = 0;
+    for (unsigned it = (peakPosition_ - psdRange_.first); it < (peakPosition_ + psdRange_.second); ++it) {
+        paraL += trace_.at(it );
+        if (it > peakPosition_ + offset) {
+            paraS += trace_.at(it );
+        }
+    }
+    return make_pair(paraS,paraL);
+};
+
 
--- ODeSA_Scan.hpp
+++ ODeSA_Scan.hpp
@@ -6,14 +6,16 @@
 #define ODESA_SCAN_CORE_H
 
 //! Standard Header Section
+#include <getopt.h>
 #include <signal.h>
 
+#include <algorithm>
+#include <iterator>
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <getopt.h>
 
-//! ROOT Header Section
+//! ROOTde Header Section
 #include "TF1.h"
 #include "TFile.h"
 #include "TH1.h"
@@ -26,34 +28,61 @@
 
 class ODeSA_Scan {
    public:
+
+     typedef struct LiquidCan{
+        Float_t l;    // Long integralP
+        Float_t s;    // Short integral
+        Float_t amp;  // Amplitude
+        Float_t ampRaw;  // Non Baseline subtracted Amplitude 
+        Float_t cfd;  // Trigger time
+        Float_t psd;  // PSD parameter s/l
+        Float_t pp;   // Peak position
+        Bool_t trig;  // Detector trigger
+    } LiquidCan; 
+
     ODeSA_Scan()=default;
 
     ~ODeSA_Scan()=default;
 
-    void ProcessFile(ifstream* file_);
+    int ProcessSingleEvent(ifstream* file_,  PulseAnalysis* Analysis_,  LiquidCan &detector, int chanNum_, double &cutTime, long &eventNum_, bool &gotMoreData, bool doTraceAnalysis  = true, pair<int,int> cfdRange_ = {1, 1}, pair<int,int> psdRange_ = {20, 200});
 
-    int OpenInputFiles(string prefix);
+    map<int,ifstream*>  OpenInputFiles(string prefix_, int NumOfFiles_, vector<int> &listOfFiles_);
 
     int ParseCmdFlags(int argc, char* argv[]);
 
-    void InitOutput(string outFile_, string outFileHeader_, int numFiles_);
+    TTree* InitOutput(string outFile_, string outFileHeader_, int numFiles_, vector<int> listOfFiles_);
+
+    int ODeSA_Cfd(pair<int,int> cfdRange_, vector<float> trace_, int &peakPosition_, float &amplitude);
+
+    int Extras_Cfd(pair<int, int> cfdRange_, vector<float> trace_, int &peakPosition_, float &amplitude);
+
+    pair<double, double> ODeSA_PSD(pair<int, int> psdRange_, vector<float> trace_, int peakPosition_, int offset = 24);
+
+    pair<double, double> Extras_PSD(pair<int, int> psdRange_, vector<float> trace_, int peakPosition_, int offset = 24);
 
     /* Set and Get Methods for various things */
     void SetNumberOfFiles(int a) { numberOfFiles_ = a; }
     void SetBatchMode(bool a) { batchMode_ = a; }
     void SetDebugMode(bool a) { debugMode_ = a; }
-    void SetOutputFileName(string a) { outName_ = a; }
+    void SetOutputFileName(string a, string b) { outName_ = a + b; }
     void SetInputFileName(string a) { inName_ = a; }
     void SetOutputFileHeader(string a) { header_ = a; }
+    void SetRunTime(double a ) { runtime = a ; }
+    void SetPreviousTime(double a) { previousTime = a; }
+    void SetVerboseTerminalErrors(bool a) {verboseErrorMode_ =a;}
 
     int GetNumberOfFiles() { return numberOfFiles_; }
     bool GetBatchMode() { return batchMode_; }
     bool GetDebugMode() { return debugMode_; }
+    bool GetVerboseTerminalErrors() {return verboseErrorMode_;}
     string GetOutputFileName() { return outName_; }
     string GetInputFileName() { return inName_; }
     string GetOutputFileHeader() { return header_; }
+    double GetRunTime() { return runtime; }
+    double GetPreviousTime() { return previousTime; }
+    map<int,LiquidCan>* GetDetectorMap() { return &detMap; }
 
-//    private:
+    //    private:
     /* Print help dialogue for command line options. */
     void ScanHelp() {
         cout << "\n SYNTAX: .\\odesa_scan [options]\n";
@@ -63,31 +92,103 @@ class ODeSA_Scan {
         cout << "  --header (-h)         | Set header\n";
         cout << "  --input (-i)          | Set input filename \n";
         cout << "  --debug (-d)          | Set debug mode (addes traces to tree)\n\n";
+        cout << "  --verbose (-v)        | Sets verbose CFD errors (default = false)\n\n";
         cout << "  --help (-?)           | Show this help\n\n\n";
     }
 
-    typedef struct {
-        Float_t l;    // Long integral
-        Float_t s;    // Short integral
-        Float_t amp;  // Amplitude
-        Float_t cfd;  // Trigger time
-        Float_t psd;  // PSD parameter s/l
-        Float_t trg;  // Detector trigger
-        Float_t pp;   // Peak position
-    } Can;
+    void PrintStartMessage(){
+//! __DATE__ and __TIME__ are compiler macros which process out to the time the binary exe is built. They are currently aligned (output takes less space than the actual macro call) 
+    cout << " ------------------------------------------------ " << endl;
+    cout << " |   ODeSA WaveDump Scan - binary version        |" << endl;
+    cout << " |   Scan Build Date: "<<__DATE__<<"                |" << endl;
+    cout << " |   Scan Build Time: "<<__TIME__<<"                   |" << endl;
+    cout << " |   Experiment: LANSCE                          |" << endl;
+    cout << " |   Exp Date: Oct 2021                          |" << endl;
+    cout << " |   Calibration used: None                      |" << endl;
+    cout << " |   ORNL Physics Division                       |" << endl;
+    cout << " ------------------------------------------------ " << endl;
+
+    }
 
     int numberOfFiles_ = 0;
-    double runtime = 0; 
+    double runtime = 0;
+    double previousTime =0 ;
+    long TEvt = 0;
 
     bool batchMode_ = false;
+    bool verboseErrorMode_ = false;
+    // bool foundBadCFDtraces
     bool debugMode_ = false;
     string outName_ = " ";
     string inName_ = " ";
     string header_ = " ";
-    vector<Can> detVec;
-    vector<TH1F> debug_Trace_vector;
-
- 
     
+    map<int,TH1F*> debug_Trace_map;
+    map<int,ODeSA_Scan::LiquidCan> detMap;
+    static vector<int> listOfFiles_;
+
+    /** ----------------------------------------------------
+     *	Channel Map used for picking which CFD to use 
+     *   ----------------------------------------------------
+     */
+
+    map<int, string> chanMap = {{0,  "RF"},
+                                {1,  "SCATTERING"},
+                                {2,  "ODESA"},
+                                {3,  "ODESA"},
+                                {4,  "ODESA"},
+                                {5,  "ODESA"},
+                                {6,  "ODESA"},
+                                {7,  "ODESA"},
+                                {8,  "ODESA"},
+                                {9,  "ODESA"},
+                                {10, "ODESA"},
+                                {11, "ODESA"},
+                                {12, "ODESA"},
+                                {13, "ODESA"},
+                                {14, "ODESA"},
+                                {15, "ODESA"}};
+
+    /** ----------------------------------------------------
+     *	Calibrations and threshold
+     *   ----------------------------------------------------
+     */
+
+    // map settings are coefficents in rising powers of X (i .e. <offset,slope>)
+    map<int, pair<float, float>> calibrations = {
+        {0,{0., 1.}},
+        {1,{0., 1.}},
+        {2,{0., 1.}},
+        {3,{0., 1.}},
+        {4,{0., 1.}},
+        {5,{0., 1.}},
+        {6,{0., 1.}},
+        {7,{0., 1.}},
+        {8,{0., 1.}},
+        {9,{0., 1.}},
+        {10,{0., 1.}},
+        {11,{0., 1.}},
+        {12,{0., 1.}},
+        {13,{0., 1.}},
+        {14,{0., 1.}},
+        {15,{0., 1.}}};
+
+    vector<float> threshold = {
+        15500,
+        15500,
+        15500,
+        15500,
+        15800,
+        15800,
+        15800,
+        15500,
+        15700,
+        15700,
+        15700,
+        15700,
+        15700,
+        15700,
+        15700,
+        15700};
 };
 #endif
